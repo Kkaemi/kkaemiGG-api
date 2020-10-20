2ff30208ec82c837765013c2b51dbaf3576835ab
SeongHyeon Ji <kitys2k3@naver.com>
2020-10-20 17:13:45 +0900

    Spring Initializr로 프로젝트 생성

---
+++ src/main/java/com/spring/kkaemiGG/KkaemiGgApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kkaemiGG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkaemiGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkaemiGgApplication.class, args);
+	}
+
+}
