ffa46a0f43680763c0274f1802586ef85ea3b6cc
SeongHyeon Ji <kitys2k3@naver.com>
2020-11-18 21:57:12 +0900

    게임 생성 일자가 모두 '1일 전'으로 뜨던 문제를 수정했습니다. 하지만 op.gg랑 fow와 값이 살짝 다르게 뜨는데 이에대한 문제는 계속 생각해 볼 필요가 있을거 같습니다

--- src/main/java/com/spring/kkaemiGG/controller/HomeController.java
+++ src/main/java/com/spring/kkaemiGG/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.spring.kkaemiGG.controller;
 
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
